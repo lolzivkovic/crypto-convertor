e5b53d2437bcd5c940e1be7654fe934ad1d03e15
Luka Zivkovic <lolzivkovic@gmail.com>
2019-05-04 22:26:30 +0200

    Modal success

--- src/components/CoinModal.js
+++ src/components/CoinModal.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 
 class CoinModal extends Component {
+
     constructor(props) {
         super(props)
 
-        this.state = {
 
+        this.state = {
+            
         }
     }
 
 
 
     render() {
-        const result = this.props.data.filter(coin => coin.id === this.props.currentCoin)
+        const result = this.props.data.filter(coin => coin.id === this.props.currentCoin).map(obj => {return `Cena je ${obj.current_price} coina ${obj.name}`})
         const showHideClass = this.props.show ? 'modal display-block' : 'modal display-none';
 
 
@@ -20,7 +22,7 @@ class CoinModal extends Component {
         return (
             <div className={showHideClass}>
                 <section className="modal-main">
-                    <p>{result.id}</p>
+                    <p>{result}</p>
                     <button onClick={this.props.hideModal}>close</button>
                 </section>
             </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ class Main extends Component {
 
         this.state = {
             data: [],
-            currentCoin: 'bitcoin',
+            currentCoin: '',
             rows: [],
             show: false
         }
