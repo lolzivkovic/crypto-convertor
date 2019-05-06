e5cc7dfabecc1dc71427d2f9c9fba49f32090128
Luka Zivkovic <lolzivkovic@gmail.com>
2019-05-07 00:07:31 +0200

    Some modal changes

--- src/components/CoinModal.js
+++ src/components/CoinModal.js
@@ -13,19 +13,19 @@ class CoinModal extends Component {
     formatPerc(percent) {
         if (!isNaN(percent)) {
             if (Math.sign(percent) === 1 || Math.sign(percent) === 0) {
-                return <td className='positiveValue'>{percent.toFixed(2)} &#37;</td>
+                return <span className='positiveValue'>{percent.toFixed(2)} &#37;</span>
             } else {
-                return <td className='negativeValue'>{percent.toFixed(2)} &#37;</td>
+                return <span className='negativeValue'>{percent.toFixed(2)} &#37;</span>
             }
         } else {
-            return <td>N/A</td>
+            return <span>N/A</span>
         }
     }
 
     render() {
 
         const data = this.props.data
-        const showHideClass = this.props.show ? 'modal display-block' : 'modal display-none';
+        const showHideClass = this.props.show ? 'modal d-flex align-items-center' : 'modal d-none';
 
         if (data.description !== undefined) {
             const perc1h = data.market_data.price_change_percentage_1h_in_currency.usd
@@ -33,41 +33,56 @@ class CoinModal extends Component {
             const perc7d = data.market_data.price_change_percentage_7d_in_currency.usd
             const perc1m = data.market_data.price_change_percentage_30d_in_currency.usd
             const perc1y = data.market_data.price_change_percentage_1y_in_currency.usd
+            const currentPrice = data.market_data.current_price.usd
 
             return (
                 <div className={showHideClass}>
-                    <section className="modal-main container">
-                        <div className='row'>
-                            <div className='col-3'>
+                    <section className='modal-main container'>
+                        <div className='m-3'>
+                            <div className='row'>
                                 <img className='logoCoin' src={data.image.large} alt={data.name} width='64' height='64'></img>
                                 <div>
                                     <h2>{data.name}</h2>
                                     <span className='modalCoinSymbol'><h5>{data.symbol.toUpperCase()}</h5></span>
                                 </div>
                             </div>
-                            <div className='col-9'>
-                                <table className='table'>
-                                    <thead>
-                                        <tr><td>1h</td><td>24h</td><td>Week</td><td>Month</td><td>Year</td></tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            {this.formatPerc(perc1h)}
-                                            {this.formatPerc(perc24h)}
-                                            {this.formatPerc(perc7d)}
-                                            {this.formatPerc(perc1m)}
-                                            {this.formatPerc(perc1y)}
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <div className='row'>
 
+                            <div className='row priceChange'>
+                                <div className='modalCurrentPrice col-12 col-md-2 d-flex flex-column justify-content- center pr-0'>
+                                    <p>Current Price</p>
+                                    <strong>{currentPrice}({this.formatPerc(perc24h)})</strong>
+                                </div>
+                                <div className="col-md-10 d-flex justify-content-around">
+                                    <div className='sparkline'>
+                                        <p>1h</p>
+                                        <strong>{this.formatPerc(perc1h)}</strong>
+                                    </div>
+                                    <div className='sparkline'>
+                                        <p>24h</p>
+                                        <strong>{this.formatPerc(perc24h)}</strong>
+                                    </div>
+                                    <div className='sparkline'>
+                                        <p>Week</p>
+                                        <strong>{this.formatPerc(perc7d)}</strong>
+                                    </div>
+                                    <div className='sparkline'>
+                                        <p>Month</p>
+                                        <strong>{this.formatPerc(perc1m)}</strong>
+                                    </div>
+                                    <div className='sparkline'>
+                                        <p>Year</p>
+                                        <strong>{this.formatPerc(perc1y)}</strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4"></div>
+                                <div className="col-8"></div>
+                            </div>
                         </div>
                         <button onClick={this.props.hideModal}>close</button>
                     </section>
-                </div>
+                </div >
             )
         } else {
             return null
--- src/components/SelectOption.js
+++ src/components/SelectOption.js
@@ -5,7 +5,7 @@ function SelectOption({ coinValue, fiatValue, handleChange, handleFiatChange, Ch
     return (
         <div className='my-3'>
             <div className='row d-flex justify-content-center align-items-center'>
-                <select className='form-control col-md-2' value={coinValue} onChange={handleChange}>
+                <select className='form-control col-sm-2' value={coinValue} onChange={handleChange}>
                     <option value='bitcoin'>BTC</option>
                     <option value='ethereum'>ETH</option>
                     <option value='ripple'>XRP</option>
@@ -18,11 +18,11 @@ function SelectOption({ coinValue, fiatValue, handleChange, handleFiatChange, Ch
                     <option value='tether'>USDT</option>
                 </select>
 
-                <input className='form-control col-md-3' id='coinVolume' type='text' onKeyUp={ChangePrice}></input>
-                <img className='col-md-auto' src='images/retweet-solid.svg' alt='Exchange Icon' width='20' height='20'></img>
-                <input className='form-control col-md-3' id='coinPrice' type='text' onKeyUp={ChangeVolume}></input>
+                <input className='form-control col-sm-3' id='coinVolume' type='text' onKeyUp={ChangePrice}></input>
+                <img className='col-sm-auto' src='images/retweet-solid.svg' alt='Exchange Icon' width='20' height='20'></img>
+                <input className='form-control col-sm-3' id='coinPrice' type='text' onKeyUp={ChangeVolume}></input>
 
-                <select className='form-control col-md-2' value={fiatValue} onChange={handleFiatChange}>
+                <select className='form-control col-sm-2' value={fiatValue} onChange={handleFiatChange}>
                     <option value='usd'>USD</option>
                     <option value='eur'>EUR</option>
                     {/* <option value='rsd'>RSD</option> */}
