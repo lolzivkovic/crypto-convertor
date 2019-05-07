72c1a555d65dd2b2b4e78a83c3239f8bf04ae018
Luka Zivkovic <lolzivkovic@gmail.com>
2019-05-07 03:17:20 +0200

    Added website link and description WIP

--- src/components/CoinModal.js
+++ src/components/CoinModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactHtmlParser from 'react-html-parser'
 
 class CoinModal extends Component {
 
@@ -21,7 +22,14 @@ class CoinModal extends Component {
             return <span>N/A</span>
         }
     }
-
+    formatWebsite(website) {
+        const re = /^(?:https?:\/\/)?(?:www\.)?/i;
+        const websites = website.filter(e => e !== '').map((e, i) => {
+            return <p key={i}><a href={e}>{e.replace(re, '').split('/')[0]}</a></p>
+        })
+        console.log(website)
+        return websites
+    }
     render() {
 
         const data = this.props.data
@@ -38,49 +46,56 @@ class CoinModal extends Component {
             return (
                 <div className={showHideClass}>
                     <section className='modal-main container'>
-                        <div className='m-3'>
-                            <div className='row'>
+                        <div className='row'>
+                            <div className="col-12">
                                 <img className='logoCoin' src={data.image.large} alt={data.name} width='64' height='64'></img>
                                 <div>
                                     <h2>{data.name}</h2>
                                     <span className='modalCoinSymbol'><h5>{data.symbol.toUpperCase()}</h5></span>
                                 </div>
-                            </div>
-
-                            <div className='row priceChange'>
-                                <div className='modalCurrentPrice col-12 col-md-2 d-flex flex-column justify-content- center pr-0'>
-                                    <p>Current Price</p>
-                                    <strong>{currentPrice}({this.formatPerc(perc24h)})</strong>
-                                </div>
-                                <div className="col-md-10 d-flex justify-content-around">
-                                    <div className='sparkline'>
-                                        <p>1h</p>
-                                        <strong>{this.formatPerc(perc1h)}</strong>
-                                    </div>
-                                    <div className='sparkline'>
-                                        <p>24h</p>
-                                        <strong>{this.formatPerc(perc24h)}</strong>
-                                    </div>
-                                    <div className='sparkline'>
-                                        <p>Week</p>
-                                        <strong>{this.formatPerc(perc7d)}</strong>
-                                    </div>
-                                    <div className='sparkline'>
-                                        <p>Month</p>
-                                        <strong>{this.formatPerc(perc1m)}</strong>
-                                    </div>
-                                    <div className='sparkline'>
-                                        <p>Year</p>
-                                        <strong>{this.formatPerc(perc1y)}</strong>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-4"></div>
-                                <div className="col-8"></div>
+                                {/* <div>
+                                    <button onClick={this.props.hideModal}>&times;</button>
+                                </div> */}
                             </div>
                         </div>
-                        <button onClick={this.props.hideModal}>close</button>
+
+                        <div className='row priceChange'>
+                            <div className='col-12 col-md-2'>
+                                <p>Current Price</p>
+                                <strong>{currentPrice}({this.formatPerc(perc24h)})</strong>
+                            </div>
+                            <div className="col-12 col-md-10 d-flex justify-content-around modalWebsite">
+                                <div className='sparkline'>
+                                    <p>1h</p>
+                                    <strong>{this.formatPerc(perc1h)}</strong>
+                                </div>
+                                <div className='sparkline'>
+                                    <p>24h</p>
+                                    <strong>{this.formatPerc(perc24h)}</strong>
+                                </div>
+                                <div className='sparkline'>
+                                    <p>Week</p>
+                                    <strong>{this.formatPerc(perc7d)}</strong>
+                                </div>
+                                <div className='sparkline'>
+                                    <p>Month</p>
+                                    <strong>{this.formatPerc(perc1m)}</strong>
+                                </div>
+                                <div className='sparkline'>
+                                    <p>Year</p>
+                                    <strong>{this.formatPerc(perc1y)}</strong>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row h-50">
+                            <div className="col-12 col-md-3 d-flex flex-column">
+                                <h3>Website</h3>
+                                {this.formatWebsite(data.links.blockchain_site)}
+                            </div>
+                            <div className="col-12 col-md-9 modalDesc">
+                                <p>{ReactHtmlParser(data.description.en)}</p>
+                            </div>
+                        </div>
                     </section>
                 </div >
             )
