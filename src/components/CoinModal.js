e7fd0da3887bafee0aefa9db63b3fbe8be8c2419
Luka Zivkovic <lolzivkovic@gmail.com>
2019-05-07 19:31:01 +0200

    Added Coin stats in modal

--- src/components/CoinModal.js
+++ src/components/CoinModal.js
@@ -5,7 +5,6 @@ class CoinModal extends Component {
 
     constructor(props) {
         super(props)
-
         this.state = {
 
         }
@@ -30,67 +29,106 @@ class CoinModal extends Component {
         console.log(website)
         return websites
     }
+    formatDate(date) {
+        const d = new Date(date)
+        return d.toDateString()
+    }
+    formatStats(stats) {
+        return stats.toLocaleString("en-US", { style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0 })
+    }
+    formatCirc(supply, symbol) {
+        return `${supply.toLocaleString()} ${symbol.toUpperCase()}`
+    }
     render() {
 
         const data = this.props.data
-        const showHideClass = this.props.show ? 'modal d-flex align-items-center' : 'modal d-none';
+        const showHideClass = this.props.show ? 'modal d-flex align-items-center justify-content-center' : 'modal d-none';
+        document.addEventListener('mousedown', this.handleOutsideClick)
 
         if (data.description !== undefined) {
             const perc1h = data.market_data.price_change_percentage_1h_in_currency.usd
-            const perc24h = data.market_data.price_change_percentage_24h_in_currency.usd
-            const perc7d = data.market_data.price_change_percentage_7d_in_currency.usd
-            const perc1m = data.market_data.price_change_percentage_30d_in_currency.usd
-            const perc1y = data.market_data.price_change_percentage_1y_in_currency.usd
+            const perc24h = data.market_data.price_change_percentage_24h
+            const perc7d = data.market_data.price_change_percentage_7d
+            const perc1m = data.market_data.price_change_percentage_30d
+            const perc1y = data.market_data.price_change_percentage_1y
             const currentPrice = data.market_data.current_price.usd
 
             return (
                 <div className={showHideClass}>
                     <section className='modal-main container'>
-                        <div className='row'>
-                            <div className="col-12">
+                        <button className='closeButton close' onClick={this.props.hideModal}><span aria-hidden="true">&times;</span></button>
+                        <div className='row modalInfo'>
+                            <div className="col-12 d-flex">
                                 <img className='logoCoin' src={data.image.large} alt={data.name} width='64' height='64'></img>
                                 <div>
                                     <h2>{data.name}</h2>
                                     <span className='modalCoinSymbol'><h5>{data.symbol.toUpperCase()}</h5></span>
                                 </div>
-                                {/* <div>
-                                    <button onClick={this.props.hideModal}>&times;</button>
-                                </div> */}
+
                             </div>
                         </div>
 
                         <div className='row priceChange'>
-                            <div className='col-12 col-md-2'>
-                                <p>Current Price</p>
-                                <strong>{currentPrice}({this.formatPerc(perc24h)})</strong>
+                            <div className='col-12 col-md-2 modalCurrentPrice'>
+                                <h5>Current Price</h5>
+                                <p>{currentPrice} ({this.formatPerc(perc24h)})</p>
                             </div>
-                            <div className="col-12 col-md-10 d-flex justify-content-around modalWebsite">
-                                <div className='sparkline'>
-                                    <p>1h</p>
-                                    <strong>{this.formatPerc(perc1h)}</strong>
+                            <div className="col-12 col-md-10 sparkline d-inline-flex justify-content-around">
+                                <div className='percs'>
+                                    <h5>1h</h5>
+                                    <p>{this.formatPerc(perc1h)}</p>
                                 </div>
-                                <div className='sparkline'>
-                                    <p>24h</p>
-                                    <strong>{this.formatPerc(perc24h)}</strong>
+                                <div className='percs'>
+                                    <h5>24h</h5>
+                                    <p>{this.formatPerc(perc24h)}</p>
                                 </div>
-                                <div className='sparkline'>
-                                    <p>Week</p>
-                                    <strong>{this.formatPerc(perc7d)}</strong>
+                                <div className='percs'>
+                                    <h5>Week</h5>
+                                    <p>{this.formatPerc(perc7d)}</p>
                                 </div>
-                                <div className='sparkline'>
-                                    <p>Month</p>
-                                    <strong>{this.formatPerc(perc1m)}</strong>
+                                <div className='percs'>
+                                    <h5>Month</h5>
+                                    <p>{this.formatPerc(perc1m)}</p>
                                 </div>
-                                <div className='sparkline'>
-                                    <p>Year</p>
-                                    <strong>{this.formatPerc(perc1y)}</strong>
+                                <div className='percs'>
+                                    <h5>Year</h5>
+                                    <p>{this.formatPerc(perc1y)}</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="row h-50">
-                            <div className="col-12 col-md-3 d-flex flex-column">
-                                <h3>Website</h3>
-                                {this.formatWebsite(data.links.blockchain_site)}
+
+                        <div className="row modalCoinStats">
+                            <div className='col-12 col-sm-6 col-md-3'>
+                                <h5>Market Cap</h5>
+                                <p>{this.formatStats(data.market_data.market_cap.usd)}</p>
+                            </div>
+                            <div className='col-12 col-sm-6 col-md-3'>
+                                <h5>All Time High</h5>
+                                <p>{this.formatStats(data.market_data.ath.usd)} ({this.formatPerc(data.market_data.ath_change_percentage.usd)})</p>
+                                <p>{this.formatDate(data.market_data.ath_date.usd)}</p>
+                            </div>
+                            <div className='col-12 col-sm-6 col-md-3'>
+                                <h5>Total Volume </h5>
+                                <p>{this.formatStats(data.market_data.total_volume.usd)}</p>
+                            </div>
+                            <div className='col-12 col-sm-6 col-md-3'>
+                                <h5>Circulating Supply</h5>
+                                <p>{this.formatCirc(data.market_data.circulating_supply, data.symbol)}</p>
+                            </div>
+                        </div>
+
+                        <div className="row modalDetails">
+                            <div className="col-12 col-md-3">
+                                <div className="row modalWebsite">
+                                    <div className='col-5 col-md-12'>
+                                        <h4>Website</h4>
+                                        {this.formatWebsite(data.links.homepage)}
+                                    </div>
+                                    <div className='col-6 col-md-12'>
+                                        <h4>Explorer</h4>
+                                        {this.formatWebsite(data.links.blockchain_site)}
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-12 col-md-9 modalDesc">
                                 <p>{ReactHtmlParser(data.description.en)}</p>
